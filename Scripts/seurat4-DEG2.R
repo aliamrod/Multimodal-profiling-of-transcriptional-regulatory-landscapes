f124220e43173de76922e12bbceb3a8c2fcbb456
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2022-12-07 15:36:33 -0600

    Update seurat4-DEG2.R

--- Scripts/seurat4-DEG2.R
+++ Scripts/seurat4-DEG2.R
@@ -2,3 +2,20 @@
 library(Seurat)
 library(BiocManager)
 library(devtools)
+
+
+
+
+
+### References ###
+# [1] "MAST: a flexible statistical framework for assessing transcriptional changes and characterizing heterogeneity in single-cell RNA sequencing data" (2015)
+# [2] "Divergent transriptional regulation of astrocyte reactivity across disorders" (2022)
+# [3] "Differential integrated stress response and asparagine production drive symbiosis and therapy resistance of pancreatic adenocarcinoma cells" (2022)
+# [4] "A novel insight into differential expression profiles of sporadic cerebral cavernouse malformation patients with different symptoms" (2021)
+# [5] "Differential transcript usage unravels gene expression alterations in Alzheimer's disease human brains" (2021)
+# [6] "A clustering-independent method for finding differentially expressed genes in single-cell transcriptome data" (2020)
+# [7] "Gene regulation by gonadal hormone receptors underlies brain sex differences" (2022)
+# [8] "Differential expression of an endogenous retroviral element [HERV-K(HML-6)] is associated with reduced survival in glioblastoma patients" (2022)
+# [9] "Single-cell and bulk transcriptome seuqencing identifies two epithelial tumor cell states and refines the consensus molecular classification of colorectal cancer" (2022)
+# [10] "Differential expression analysis of genes and long non-coding RNAs associated with KRAS mutation in colorectal cancer cells" (2022)
+# [11] "The landscape of tumor cell states and spatial organization in H3-K27M mutant diffuse midline glioma across age and location" (2022)
