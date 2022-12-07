308f67aff4a0e2f3160c4afd7e7608a486280c55
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2022-12-07 15:16:45 -0600

    Update and rename seurat3-DE.R to seurat3-DEG1.R

---
+++ Scripts/seurat3-DEG1.R
@@ -0,0 +1,13 @@
+# Read in table
+if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install("SingleCellExperiment")
+library(SingleCellExperiment)
+library(Seurat)
+
+# Create Seurat object using scran object.
+
+
+
+
+*** update dec 7
